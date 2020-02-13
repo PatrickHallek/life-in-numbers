81c858721f26c4964d3d377bba802e3f91c0dd74
PatrickHallek <patrickhallek1998@gmail.com>
2020-02-13 13:45:11 +0100

    Add card component

---
+++ frontend/src/components/layouts/appCard.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Typography, Box, Card, CardContent } from "@material-ui/core";
+
+type AppCardProps = {
+  title: string,
+  handler: any
+}
+
+const AppCard = ({ title, handler }: AppCardProps) => {
+
+  return (
+    <Box mt={3}>
+      <Card>
+        <CardContent>
+          <Typography variant="h3" component="h2">
+            <Box fontWeight="fontWeightBold"></Box>
+          </Typography>
+          <Typography variant="h4" component="h3">
+            {title}
+          </Typography>
+          <Button onClick={handler} variant="contained" color="primary" className="button">
+            Submit
+          </Button>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+}
+
+export default AppCard;
