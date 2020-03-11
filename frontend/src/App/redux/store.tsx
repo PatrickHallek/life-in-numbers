4db46243ac741139a6297a1bf580fe7ee379cf2b
Hallek <paha@MACEX314.local>
2020-03-11 14:40:17 +0100

    Add redux devtools middleware

--- frontend/src/App/redux/store.tsx
+++ frontend/src/App/redux/store.tsx
@@ -1,11 +1,27 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
 import contentReducer from "./reducer/contentReducer";
+import thunk from "redux-thunk"
 import { answerReducer } from "./reducer/answerReducer";
 import { resultReducer } from "./reducer/resultReducer";
 
-const rootReducer = combineReducers({ content: contentReducer, result: resultReducer, answers: answerReducer })
 
-const store = createStore(rootReducer)
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
+const rootReducer = combineReducers({ content: contentReducer, result: resultReducer, answers: answerReducer })
+const middleware = [thunk]
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(...middleware),
+        composeEnhancers()
+    )
+)
 
 
 export default store;
