56a3ecce91f7ac18381b0edf5db60c484f4517e4
Hallek <paha@MACEX314.fritz.box>
2020-03-11 20:10:49 +0100

    Make result screen responsive

--- frontend/src/App/components/features/result.tsx
+++ frontend/src/App/components/features/result.tsx
@@ -11,14 +11,14 @@ const Result = () => {
 
     const renderAverageValue = () => {
         if (result.averageValue)
-            return <Box mt="50px" fontSize="36px" width="100%">*The average person burned {averageValue} at your age</Box>
+            return <Box mt="50px" mb="50px" fontSize="36px" width="100%">*The average person burned {averageValue} at your age</Box>
         else return
     }
 
     if (Number(value) !== 0) return (
         <Box display="flex" justifyContent="center" flexWrap="wrap">
-            <Box fontSize="68px" fontWeight="bold" color={content.theme.primary}>{content.resultText}</Box>
-            <Box fontSize="90px" fontWeight="bold" color={content.theme.primary}>{value}</Box>
+            <Box className="result" fontWeight="bold" color={content.theme.primary}>{content.resultText}</Box>
+            <Box className="result" fontWeight="bold" color={content.theme.primary}>{value}</Box>
             {renderAverageValue()}
         </Box>
     )
